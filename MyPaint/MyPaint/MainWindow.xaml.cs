c5024466704e1b4d7a1ec3609ca2511faea17d9f
sSergey2017 <eney05@i.ua>
2017-06-17 20:56:51 +0300

    Самій простой paint на канвасе

---
+++ MyPaint/MyPaint/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyPaint
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //вінести  в соответствующие папки
+            mcolor = new ColorRGB();
+            mcolor.red = 0;
+            mcolor.green = 0;
+            mcolor.blue = 0;
+            this.lbl1.Background = new SolidColorBrush(Color.FromRgb(mcolor.red, mcolor.green, mcolor.blue));
+        }
+
+        private void buttonClear_Click(object sender, RoutedEventArgs e)
+        {
+            this.inkCanvas1.Strokes.Clear();
+        }
+
+        private void buttonClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonSave_Click(object sender, RoutedEventArgs e)
+        {
+            string imgPath = @"D:\Lessons\wpfLessons\file.bmp"; //Куда сохраняется файл
+            MemoryStream ms = new MemoryStream();  //Поток памяти :)
+            FileStream fs = new FileStream(imgPath, FileMode.Create); //  Поток файла :)
+
+            //rtb - объект класса RenderTargetBitmap
+            RenderTargetBitmap rtb = new RenderTargetBitmap((int)inkCanvas1.Width, (int)inkCanvas1.Height, 96, 96, PixelFormats.Default);
+            rtb.Render(inkCanvas1);
+
+            //GifBitmapEncoder gifEnc = new GifBitmapEncoder(); //сохраняем в формате GIF
+            BmpBitmapEncoder bmpEnc = new BmpBitmapEncoder();
+            bmpEnc.Frames.Add(BitmapFrame.Create(rtb));
+            bmpEnc.Save(fs);
+            fs.Close();
+            MessageBox.Show("Файл сохранен, " + imgPath); //Для информации
+        }
+
+        private void sld_Color_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var slider = sender as Slider;
+            string crlName = slider.Name; //Определяем имя контрола, который покрутили
+            double value = slider.Value; // Получаем значение контрола
+            //В зависимости от выбранного контрола, меняем ту или иную компоненту и переводим ее в тип byte
+            if (crlName.Equals("sld_RedColor"))
+            {
+                mcolor.red = Convert.ToByte(value);
+            }
+            if (crlName.Equals("sld_GreenColor"))
+            {
+                mcolor.green = Convert.ToByte(value);
+            }
+            if (crlName.Equals("sld_BlueColor"))
+            {
+                mcolor.blue = Convert.ToByte(value);
+            }
+
+            //Задаем значение переменной класса clr 
+            clr = Color.FromRgb(mcolor.red, mcolor.green, mcolor.blue);
+            //Устанавливаем фон для контрола Label 
+            this.lbl1.Background = new SolidColorBrush(Color.FromRgb(mcolor.red, mcolor.green, mcolor.blue));
+            // Задаем цвет кисти для контрола InkCanvas
+            this.inkCanvas1.DefaultDrawingAttributes.Color = clr;
+        }
+
+        // для примера, все рядом
+        public class ColorRGB
+        {
+            public byte red { get; set; }
+            public byte green { get; set; }
+            public byte blue { get; set; }
+        }
+
+        public ColorRGB mcolor { get; set; }
+
+        public Color clr { get; set; }
+
+
+        private void sld_Width_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var slider = sender as Slider;
+            string crlName = slider.Name; //Определяем имя контрола, который покрутили
+            double value = slider.Value; // Получаем значение контрола
+            this.inkCanvas1.DefaultDrawingAttributes.Width = value;
+        }
+    }
+}
